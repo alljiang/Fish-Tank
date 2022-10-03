53e62b0359eb7113378cb4c9c40e9bf904b514f8
Allen Jiang <alljiang@hotmail.com>
2022-10-03 00:55:24 -0500

    Add ino

---
+++ Arduino-Motor-Controller/Arduino-Motor-Controller.ino
@@ -0,0 +1,22 @@
+#define EN 8        // stepper motor enable, active low
+#define X_DIR 5     // X - axis stepper motor direction control
+#define Y_DIR 6     // y - axis stepper motor direction control
+#define Z_DIR 7     // z axis stepper motor direction control
+#define X_STP 2     // x - axis stepper control
+#define Y_STP 3     // y - axis stepper control
+#define Z_STP 4     // z - axis stepper control
+
+#define RX 0
+#define TX 1
+#define X_LIM 9     // input - pull up
+#define Y_LIM 10    // input - pull up
+#define Z_LIM 11    // input - pull up
+#define SPINDLE_ENABLE 12   
+#define SPINDLE_DIRECTION 13
+
+#define RESET_ABORT A0 // input - pull up
+#define FEED_HOLD A1   // input - pull up
+#define CYCLE_START A2 // input - pull up
+#define COOLANT_ENABLE A3 // input - pull up
+#define SCL A4          // input - pull up
+#define SDA A5          // input - pull up
