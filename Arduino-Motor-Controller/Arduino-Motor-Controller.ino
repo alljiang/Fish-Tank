aea7ea33dc22449d3698eeb561a7262b6fe9d641
Allen Jiang <alljiang@hotmail.com>
2022-10-04 05:47:36 -0500

    choose pins

--- Arduino-Motor-Controller/Arduino-Motor-Controller.ino
+++ Arduino-Motor-Controller/Arduino-Motor-Controller.ino
@@ -1,9 +1,11 @@
 
 
-
 /* Hardware Configuration:
     - Add jumper to set SpinEnable to A-STEP
     - Add jumper to set SpinDir to A-DIR
+    - Add jumper between X.STEP and X.ENDSTOP
+    - Add jumper between A.STEP and Y.ENDSTOP
+    - Add jumper between Z.STEP and Z.ENDSTOP
     - Configure max current potentiometer on each driver to 0.8A
         https://ardufocus.com/howto/a4988-motor-current-tuning/
         Make sure to confirm sensing resitor value
@@ -29,23 +31,23 @@
 #define PIN_A_DIR 13   // FR
 #define PIN_X_DIR 5    // BL
 #define PIN_Z_DIR 7    // BR
-#define PIN_Y_STP 3
-#define PIN_A_STP 12
-#define PIN_X_STP 2
-#define PIN_Z_STP 4
+#define PIN_Y_STP 3    // FL, PWM
+#define PIN_A_STP 12   // Overriden
+#define PIN_X_STP 2    // Overriden
+#define PIN_Z_STP 4    // Overriden
 
 #define PIN_RX 0 // Serial
 #define PIN_TX 1 // Serial
 
-#define PIN_X_LIM 9           // input
-#define PIN_Y_LIM 10          // input
-#define PIN_Z_LIM 11          // input
-#define PIN_RESET_ABORT A0    // input
-#define PIN_FEED_HOLD A1      // input
-#define PIN_CYCLE_START A2    // input
-#define PIN_COOLANT_ENABLE A3 // input
-#define PIN_SCL A4            // input
-#define PIN_SDA A5            // input
+#define PIN_X_LIM 9           // BL, PWM
+#define PIN_Y_LIM 10          // FR, PWM
+#define PIN_Z_LIM 11          // BR, PWM
+#define PIN_RESET_ABORT A0    // NC
+#define PIN_FEED_HOLD A1      // NC
+#define PIN_CYCLE_START A2    // NC
+#define PIN_COOLANT_ENABLE A3 // NC
+#define PIN_SCL A4            // NC
+#define PIN_SDA A5            // Using as Neopixel Data
 
 // Neopixels
 #define PIN PIN_SDA
@@ -54,8 +56,31 @@
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
+  pinMode(PIN_A4988_EN, OUTPUT);
+  pinMode(PIN_Y_DIR, OUTPUT);
+  pinMode(PIN_A_DIR, OUTPUT);
+  pinMode(PIN_X_DIR, OUTPUT);
+  pinMode(PIN_Z_DIR, OUTPUT);
+  pinMode(PIN_Y_STP, OUTPUT);
 
+  // these are overriden
+  pinMode(PIN_A_STP, INPUT);
+  pinMode(PIN_X_STP, INPUT);
+  pinMode(PIN_Z_STP, INPUT);
+
+  // these PWM pins override the motor step pins
+  pinMode(PIN_X_LIM, OUTPUT);
+  pinMode(PIN_Y_LIM, OUTPUT);
+  pinMode(PIN_Z_LIM, OUTPUT);
+
+  pinMode(PIN_RESET_ABORT, INPUT);
+  pinMode(PIN_FEED_HOLD, INPUT);
+  pinMode(PIN_CYCLE_START, INPUT);
+  pinMode(PIN_COOLANT_ENABLE, INPUT);
+  pinMode(PIN_SCL, INPUT);
+  pinMode(PIN_SDA, INPUT);
+
+  digitalWrite(PIN_A4988_EN, LOW);
 }
 
-void loop() {
-}
+void loop() {}
