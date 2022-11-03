cb6deda543d4232878b54e186bf5bab9b1ec8aee
Allen Jiang <alljiang@hotmail.com>
2022-11-03 01:52:58 -0500

    microstepping

--- Python-UART-Test/wasd-controller.py
+++ Python-UART-Test/wasd-controller.py
@@ -6,16 +6,16 @@ while True:
     print("\nEnter a command")
     command = input("Command: ")
     if command == "w":
-        controller.send_velocity(50, 0, 0)
+        controller.send_velocity(200, 0, 0)
     elif command == "s":
-        controller.send_velocity(-50, 0, 0)
+        controller.send_velocity(-200, 0, 0)
     elif command == "a":
-        controller.send_velocity(0, -50, 0)
+        controller.send_velocity(0, -200, 0)
     elif command == "d":
-        controller.send_velocity(0, 50, 0)
+        controller.send_velocity(0, 200, 0)
     elif command == "q":
-        controller.send_velocity(0, 0, -50)
+        controller.send_velocity(0, 0, -200)
     elif command == "e":
-        controller.send_velocity(0, 0, 50)
+        controller.send_velocity(0, 0, 200)
     else:
         controller.send_velocity(0, 0, 0)
