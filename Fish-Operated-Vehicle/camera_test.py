851785c2e863a011cd705973f2f85f5869ce8ffa
Allen Jiang <alljiang@hotmail.com>
2022-11-03 04:34:25 -0500

    sender stuff

--- Fish-Operated-Vehicle/camera_test.py
+++ Fish-Operated-Vehicle/camera_test.py
@@ -9,11 +9,14 @@ import patience
 
 cap = cv2.VideoCapture(0)
 
-sender = imagezmq.ImageSender(connect_to='tcp://Allen-PC:5555')
-sender.zmq_socket.setsockopt(zmq.LINGER, 0)  # prevents ZMQ hang on exit
-sender.zmq_socket.setsockopt(zmq.RCVTIMEO, 1000)
-sender.zmq_socket.setsockopt(zmq.SNDTIMEO, 1000)
+def create_sender():
+    sender = imagezmq.ImageSender(connect_to='tcp://Allen-PC:5555')
+    sender.zmq_socket.setsockopt(zmq.LINGER, 0)  # prevents ZMQ hang on exit
+    sender.zmq_socket.setsockopt(zmq.RCVTIMEO, 1000)
+    sender.zmq_socket.setsockopt(zmq.SNDTIMEO, 1000)
+    return sender
 
+sender = create_sender()
 rpi_name = socket.gethostname()  # send RPi hostname with each image
 
 while(True):
@@ -23,3 +26,6 @@ while(True):
     except:
         print("Error")
         time.sleep(1)
+
+        # reset sender
+        sender = create_sender()
