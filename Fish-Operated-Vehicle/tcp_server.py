7199932159a92ce82d21412e499399641bf2ed63
Allen Jiang <alljiang@hotmail.com>
2022-11-03 14:59:12 -0500

    fix hostname

--- Fish-Operated-Vehicle/tcp_server.py
+++ Fish-Operated-Vehicle/tcp_server.py
@@ -8,12 +8,11 @@ class Server:
 
     def server_start(self):
         # get the hostname
-        host = "localhost"
         port = 5000  # initiate port no above 1024
 
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
         # look closely. The bind() function takes tuple as argument
-        server_socket.bind((host, port))  # bind host address and port together
+        server_socket.bind(('', port))  # bind host address and port together
 
         # configure how many client the server can listen simultaneously
         server_socket.listen(1)
