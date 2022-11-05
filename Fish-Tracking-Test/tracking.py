a34b114ab3d9a89a7462c7b044a1859020af1c8c
Allen Jiang <alljiang@hotmail.com>
2022-11-05 06:03:47 -0500

    doing tracking

--- Fish-Tracking-Test/tracking.py
+++ Fish-Tracking-Test/tracking.py
@@ -2,14 +2,12 @@ import cv2
 import numpy as np
 
 def modify_frame(frame):
-    frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
-
     # convert to hsv
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define threshold
-    lower = np.array([40, 0, 0])
-    upper = np.array([255, 255, 60])
+    lower = np.array([0, 0, 0])
+    upper = np.array([255, 255, 80])
 
     mask = cv2.inRange(hsv, lower, upper)
 
@@ -19,19 +17,29 @@ def modify_frame(frame):
     result = cv2.bitwise_and(frame, frame, mask=mask)
     return result
 
-INPUT_VIDEO = "Fish-Tracking-Test\\input\\bozo.mp4"
+# INPUT_VIDEO = "Fish-Tracking-Test\\red-static.mp4"
+INPUT_VIDEO = "Fish-Tracking-Test\\red-moving.mp4"
 
 # Read video
 cap = cv2.VideoCapture(INPUT_VIDEO)
 
-new_video = cv2.VideoWriter("Fish-Tracking-Test\\output\\output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 30, (640, 480))
+new_video = cv2.VideoWriter("Fish-Tracking-Test\\output\\red-static.avi", cv2.VideoWriter_fourcc(*"MJPG"), 30, (640, 480))
 
-for i in range(0, 100):
+for i in range(0, 2000):
     ret, frame = cap.read()
     if ret:
-        cv2.imshow("frame", frame)
+        frame_original = frame.copy()
         frame = modify_frame(frame)
-        new_video.write(frame)
+        # combine the two frames
+        combined = np.concatenate((frame_original, frame), axis=1)
+        cv2.imshow("combined", combined)
+        # new_video.write(frame)
+        # wait for space to be pressed
+        print("Press space to continue")
+        if cv2.waitKey(0) & 0xFF == ord(' '):
+            continue
+        else:
+            break
     else:
         break
 
