6c3ea85dba4b6f99d179adcd6e3364668aa0b9eb
Allen Jiang <alljiang@hotmail.com>
2022-11-05 03:07:39 -0500

    recorder

--- Fish-Tracking-Test/recorder.py
+++ Fish-Tracking-Test/recorder.py
@@ -8,18 +8,22 @@ CAPTURE_DURATION = 10
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
 cap.set(cv2.CAP_PROP_FPS, 30)
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output.mp4',fourcc, 30.0, (1920, 1080))
 
+frame_count = 0
 start_time = time.time()
 print("Recording started")
 while( int(time.time() - start_time) < CAPTURE_DURATION ):
+    print(time.time())
     ret, frame = cap.read()
     if ret == True:
         out.write(frame)
+        frame_count += 1
 
 #        cv2.imshow('frame',frame)
 #        if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -29,6 +33,10 @@ while( int(time.time() - start_time) < CAPTURE_DURATION ):
 
 # Release everything if job is finished
 print("Recording finished")
+
+new_fps = frame_count / CAPTURE_DURATION
+out.set(cv2.CAP_PROP_FPS, new_fps)
+
 cap.release()
 out.release()
 cv2.destroyAllWindows()
